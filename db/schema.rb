5676e00e4dd00e2fb6197b9515ecb75ccc544776
Martin Hartung <hartung_martin@gmx.de>
2020-05-25 15:22:26 +0000

    Added Profiles Table and link to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200525091854) do
+ActiveRecord::Schema.define(version: 20200525141525) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 20200525091854) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "stripe_plan_token"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_numbers"
+    t.string   "contact_mail"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
